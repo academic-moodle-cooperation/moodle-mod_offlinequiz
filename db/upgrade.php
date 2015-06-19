1942de1edba0d844392fe9e45fb76c951c9d5b98
Juergen Zimmer <juergen.zimmer@univie.ac.at>
2015-06-19 08:06:21 +0200

    One transaction for each offlinequiz.

--- db/upgrade.php
+++ db/upgrade.php
@@ -1153,12 +1153,15 @@ function xmldb_offlinequiz_upgrade($oldversion = 0) {
             $pbar->update(0, $numofflinequizzes,
                         "Upgrading offlinequiz group questions - {0}/{$numofflinequizzes}.");
 
-
+            flush();
+            ob_flush();
+            
             $numberdone = 0;
             $offlinequizzes = $DB->get_recordset('offlinequiz', null, 'id', 'id, numgroups');
             foreach ($offlinequizzes as $offlinequiz) {
                 $transaction = $DB->start_delegated_transaction();
-    
+                error_log('Upgrading offlinequiz ' . $offlinequiz->id . ' to version 2015060900.');
+
                 $groups = $DB->get_records('offlinequiz_groups', array('offlinequizid' => $offlinequiz->id),
                         'number', '*', 0, $offlinequiz->numgroups);
                 $questioninstances = $DB->get_records('offlinequiz_q_instances',
