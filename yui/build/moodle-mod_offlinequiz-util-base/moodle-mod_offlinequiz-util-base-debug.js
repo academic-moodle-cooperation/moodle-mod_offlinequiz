9b4ea47f5e7a18837534078d09fac2164e03bb00
Markus Kemmerling <markus@Markus-iMac.local>
2015-09-22 17:58:43 +0200

    Added missing yui/build directory for version 2015060902.

---
+++ yui/build/moodle-mod_offlinequiz-util-base/moodle-mod_offlinequiz-util-base-debug.js
@@ -0,0 +1,20 @@
+YUI.add('moodle-mod_offlinequiz-util-base', function (Y, NAME) {
+
+/**
+ * The Moodle.mod_offlinequiz.util classes provide offlinequiz-related utility functions.
+ *
+ * @module moodle-mod_offlinequiz-util
+ * @main
+ */
+
+Y.namespace('Moodle.mod_offlinequiz.util');
+
+/**
+ * A collection of general utility functions for use in offlinequiz.
+ *
+ * @class Moodle.mod_offlinequiz.util
+ * @static
+ */
+
+
+}, '@VERSION@');
---
+++ yui/build/moodle-mod_offlinequiz-util-base/moodle-mod_offlinequiz-util-base-min.js
@@ -0,0 +1,1 @@
+YUI.add("moodle-mod_offlinequiz-util-base",function(e,t){e.namespace("Moodle.mod_offlinequiz.util")},"@VERSION@");
