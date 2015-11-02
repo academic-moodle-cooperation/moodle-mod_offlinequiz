02a076f4cf8f95d2dcc051f93c5300393a355706
Juergen Zimmer <juergen.zimmer@univie.ac.at>
2015-11-02 11:48:24 +0100

    Redmine 2767: try to fix display error.

--- review.php
+++ review.php
@@ -138,10 +138,12 @@ if (!empty($offlinequiz->time)) {
 if ($options->marks != question_display_options::HIDDEN) {
     if ($offlinequiz->grade and $group->sumgrades) {
         $a = new stdClass;
-        $percentage = format_float(($result->sumgrades / $group->sumgrades) * 100, $offlinequiz->decimalpoints);
         $a->grade = format_float(preg_replace('/,/i', '.', $grade), $offlinequiz->decimalpoints);
         $a->maxgrade = format_float($offlinequiz->grade, $offlinequiz->decimalpoints);
+
         $rawscore = format_float($result->sumgrades, $offlinequiz->decimalpoints);
+        $percentage = format_float(($result->sumgrades * 1.0 / $group->sumgrades) * 100, $offlinequiz->decimalpoints);
+
         $table->data[] = array($strscore . ':', $rawscore . '/' . format_float($group->sumgrades,
                 $offlinequiz->decimalpoints) . ' (' . $percentage . '%)');
         $table->data[] = array($strgrade . ':', get_string('outof', 'offlinequiz', $a));
