febb860c17d912617783d9e49b321c6f2701c323
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2017-02-02 09:35:55 +0100

    redmine 3733: change changelog for moodle_32

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017013000;
+$plugin->version   = 2017020200;
 $plugin->requires  = 2016120500.00;
 $plugin->cron      = 3600;
 $plugin->component = 'mod_offlinequiz';
