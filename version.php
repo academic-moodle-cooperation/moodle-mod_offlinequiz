06117f772af4ffe3e86959510f05d7710493e2ed
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2017-05-03 17:52:46 +0200

    redmine-4403: fix missing backup-step

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017020203;
+$plugin->version   = 2017020204;
 $plugin->requires  = 2016120500;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
