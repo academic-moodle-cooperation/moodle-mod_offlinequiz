fab8e7c54198a6f5a6890803bbab340f387ddc29
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2018-04-10 09:42:32 +0200

    prepare version 3.4.2

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018011601;
+$plugin->version   = 2018011602;
 $plugin->requires  = 2017111300;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.4.1";
+$plugin->release   = "v3.4.2";
