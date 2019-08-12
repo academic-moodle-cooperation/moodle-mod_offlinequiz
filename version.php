9fb003e0ae54784de459975720e303e176b5361c
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2019-08-12 08:27:39 +0200

    moodle version upgraded

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2019080700;
-$plugin->requires  = 2018051700;
+$plugin->requires  = 2019052000;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = "v3.7.0";
