911e0641a49264b21c0d840d0c0de447f964e0b0
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2022-05-30 13:26:04 +0200

    fix questionbank classname changes

--- classes/question/bank/checkbox_column.php
+++ classes/question/bank/checkbox_column.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
  * @since         Moodle 2.8+
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class checkbox_column extends \core_question\bank\checkbox_column {
+class checkbox_column extends \core_question\local\bank\checkbox_column {
     protected $strselect;
 
     protected function display_content($question, $rowclasses): void {
