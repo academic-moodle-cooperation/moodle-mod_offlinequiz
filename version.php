51945aa9b57a7e6740605859c04e1059193d9328
Moodle Entwicklung <moodle-dev@univie.ac.at>
2022-11-02 13:45:24 +0100

    update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022080100;
+$plugin->version   = 2022110200;
 $plugin->requires  = 2021050700;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_ALPHA;
