469e9bd7fefbf5da026d7f1ed55de9502819372e
Adrian Czermak <adrian.czermak@univie.ac.at>
2023-07-10 13:20:37 +0000

    Develop 401

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023070700;
+$plugin->version   = 2023070701;
 $plugin->requires  = 2022112800;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
