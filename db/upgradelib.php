38465ad22e7570b6b076fa2feea1a73bd34db131
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2023-11-27 14:23:35 +0100

    fix 2

--- db/upgradelib.php
+++ db/upgradelib.php
@@ -1112,7 +1112,7 @@ function offlinequiz_fix_question_versions() {
         $DB->set_field('question_attempts', 'questionid', $record->newquestionid, ['questionid' => $record->oldquestionid, 'questionusageid' => $templateusage->get_id()]);
     }
 
-    $sql = "SELECT ogq.id itemid, c.id usingcontextid, 'mod_offlinequiz' component, 'slot' questionarea,  qv.questionbankentryid questionbankentryid, qv.\"version\" version
+    $sql = "SELECT ogq.id itemid, c.id usingcontextid, 'mod_offlinequiz' component, 'slot' questionarea,  qv.questionbankentryid questionbankentryid, qv.\"version\" \"version\"
               FROM {offlinequiz_group_questions} ogq
               JOIN {modules} m ON m.name ='offlinequiz' 
               JOIN {course_modules} cm ON cm.module = m.id AND cm.instance = ogq.offlinequizid 
