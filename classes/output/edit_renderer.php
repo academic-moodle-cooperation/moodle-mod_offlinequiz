dbe1b1b760e4c370684622d148d156c5532d1032
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2023-12-12 14:08:15 +0100

    fix menu

--- classes/output/edit_renderer.php
+++ classes/output/edit_renderer.php
@@ -627,8 +627,6 @@ class edit_renderer extends \plugin_renderer_base {
             return '';
         }
         $menu = new \action_menu();
-        $menu->set_menu_left();
-        $menu->set_constraint('.mod-offlinequiz-edit-content');
         $trigger = html_writer::tag('span', get_string('add'), array('class' => 'add-menu'));
         $menu->set_menu_trigger($trigger);
         // The menu appears within an absolutely positioned element causing width problems.
