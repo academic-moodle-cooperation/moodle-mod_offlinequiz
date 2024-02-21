36363e5f3239409891e2e9c3511778106488da77
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2024-02-21 16:12:19 +0100

    fix 1660: upgrade gradebook after changing question versions

--- locallib.php
+++ locallib.php
@@ -850,6 +850,8 @@ function offlinequiz_update_question_instance($offlinequiz, $contextid, $questio
             }
         }
     }
+    $DB->delete_records('offlinequiz_statistics', ['offlinequizid' => $offlinequiz->id]);
+    offlinequiz_update_grades($offlinequiz);
     $DB->commit_delegated_transaction($transaction);
 }
 
