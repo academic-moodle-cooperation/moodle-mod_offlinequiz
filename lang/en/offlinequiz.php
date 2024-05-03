66aab7c87aca61f4dbacacea33423f8f2adbb4c8
Martin Reichel <martin.w.reichel@univie.ac.at>
2024-05-03 11:32:11 +0000

    Update 2 files
    - /CHANGELOG.md
    - /lang/en/offlinequiz.php

--- lang/en/offlinequiz.php
+++ lang/en/offlinequiz.php
@@ -833,6 +833,7 @@ $string['statisticsviewheader'] = '3. Statistics';
 $string['statisticsplural'] = 'Statistics';
 $string['statsoverview'] = 'Statistics overview';
 $string['studycode'] = 'Study code';
+$string['subplugintype_offlinequiz_plural'] = 'Offlinequiz reports';
 $string['tabattendances'] = 'Attendances';
 $string['tabattendancesoverview'] = 'Attendances';
 $string['tabdownloadquizforms'] = 'Forms';
