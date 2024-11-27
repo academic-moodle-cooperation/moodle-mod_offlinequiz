10487ee88b8a7f021d123adb7ea7d0b096ee0021
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2024-11-27 18:00:01 +0100

    fix adhoc tasks

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2024072900;
+$plugin->version   = 2024073000;
 $plugin->requires  = 2024042200;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
