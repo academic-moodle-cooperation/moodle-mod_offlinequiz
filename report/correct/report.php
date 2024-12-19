c848dd3b4c74243257d93eff2022204d356aa35e
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2024-12-19 13:03:05 +0100

    fix display queue file list

--- report/correct/report.php
+++ report/correct/report.php
@@ -355,10 +355,13 @@ class offlinequiz_correct_report extends offlinequiz_default_report {
                     $filecontext['evaluated'] = true;
                 }
                 if($page->status == 'ok') {
-                    $filecontext['done'] = true;
-                }
-                if($page->status == 'error') {
-                    $filecontext['error'] = true;
+                    $filecontext['filestatusdone'] = true;
+                } else if($page->status == 'error') {
+                    $filecontext['filestatuserror'] = true;
+                } else if($page->status == 'new'){
+                    $filecontext['filestatusnotstarted'] = true;
+                } else {
+                    $filecontext['filestatusprocessing'] = true;
                 }
                 if (!empty($page->userkey) && $page->userkey) {
                     //TODO get userkey
