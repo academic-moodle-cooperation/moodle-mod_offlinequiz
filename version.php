cb07b365d84cf51d6d78b4972b6c6ee68c538426
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2025-01-15 13:39:51 +0100

    change version

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2024073001;
+$plugin->version   = 2025015000;
 $plugin->requires  = 2024042200;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v4.4.0";
+$plugin->release   = "v4.5.0";
