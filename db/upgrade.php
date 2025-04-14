ffa86894899120383acf4d698b62187eefb7392f
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2025-04-14 10:32:16 +0200

    fix missing pdffont for newer installations

--- db/upgrade.php
+++ db/upgrade.php
@@ -1707,6 +1707,19 @@ function xmldb_offlinequiz_upgrade($oldversion = 0) {
         // Offlinequiz savepoint reached.
         upgrade_mod_savepoint(true, 2024043002, 'offlinequiz');
     }
-
+    if ($oldversion < 2024071202) {
+        
+        // Define field pdffont to be added to offlinequiz.
+        $table = new xmldb_table('offlinequiz');
+        $field = new xmldb_field('pdffont', XMLDB_TYPE_CHAR, '255', null, null, null, null, 'printstudycodefield');
+        
+        // Conditionally launch add field pdffont.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        
+        // Offlinequiz savepoint reached.
+        upgrade_mod_savepoint(true, 2024071202, 'offlinequiz');
+    }
     return true;
 }
--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2024071201;
+$plugin->version   = 2024071202;
 $plugin->requires  = 2024100700;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v4.5.1";
+$plugin->release   = "v4.5.2";
