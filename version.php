097aa182e7fd58913959ee4e26cf8f9e78f8c801
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2026-06-10 16:22:23 +0200

    fix version.php

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2024071203;
+$plugin->version   = 2024071204;
 $plugin->requires  = 2024100700;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
